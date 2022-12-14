9a16361248912fd1b15935dce2b1f411aa205b05
Nikolai Orlov <nicolasorlov174@yandex.com>
2022-12-14 16:30:47 +0600

    first srializer

---
+++ backend/foodgram/recipes/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from .models import Recipes
+
+
+class RecipesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recipes
+        fields = '__all__' 
--- backend/foodgram/recipes/views.py
+++ backend/foodgram/recipes/views.py
@@ -1,8 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from .models import Recipes
 from .paginator import paginate_page
+from .serializers import RecipesSerializer
 
 
 def index(request):
@@ -17,8 +21,18 @@ def index(request):
     return HttpResponse('Recipes list')
 
 
+@api_view(['GET', 'POST'])
 def recipes_list(request):
-    return HttpResponse('Recipes list')
+    if request.method == 'POST':
+        serializer = RecipesSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    recipes = Recipes.objects.all()
+    serializer = RecipesSerializer(recipes, many=True)
+    return Response(serializer.data) 
+    #return HttpResponse('Recipes list')
 
 
 def recipes_detail(request, pk):
